a18084349042bfc8eea3814f1927213223523e8c
wangdezheng <wang793013053@163.com>
2017-11-03 11:45:03 -0700

    Largest SubArray Sum

---
+++ Algorithm_Category/DynamicProgramming/LargestSumOfSubarray.java
@@ -0,0 +1,23 @@
+package DynamicProgramming;
+
+/**
+ * Given an unsorted integer array, find the subarray that has the greatest sum. Return the sum.
+ * <p>
+ * Assumptions
+ * <p>
+ * The given array is not null and has length of at least 1.
+ */
+//Time: O(n), Space: O(1)
+public class LargestSumOfSubarray {
+    public int largestSum(int[] array) {
+        // sum[i] represents the greateset sum of subarray from 0th position to ith position (include i)
+        int[] sum = new int[array.length];
+        sum[0] = array[0];
+        int lastMax = sum[0];
+        for (int i = 1; i < array.length; i++) {
+            sum[i] = Math.max(array[i], sum[i - 1] + array[i]);
+            lastMax = Math.max(lastMax, sum[i]);
+        }
+        return lastMax;
+    }
+}
