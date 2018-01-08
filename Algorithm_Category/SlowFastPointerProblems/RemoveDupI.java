8847e6f9a6427bda99ad130142f9bc59d5a41336
wangdezheng <wang793013053@163.com>
2018-01-08 23:04:04 +0800

    remove duplicated elements in an array and keep only one of them

---
+++ Algorithm_Category/SlowFastPointerProblems/RemoveDupI.java
@@ -0,0 +1,26 @@
+package SlowFastPointerProblems;
+
+/**
+ * Given a sorted array, remove duplicated elements in it and keep only one of them, return the length of result
+ */
+
+
+//Time: O(n), Space: O(1)
+//slow: all elements to the left side of slow(included) are the final results to return
+//fast: current index
+public class RemoveDupI {
+    public static int removeDup(int[] input) {
+        if (input == null || input.length == 0) {
+            return 0;
+        }
+        int slow = 0;
+
+        for (int fast = 1; fast < input.length; fast++) {
+            if (input[slow] == input[fast]) {
+                continue;
+            }
+            input[++slow] = input[fast];
+        }
+        return slow + 1;
+    }
+}
