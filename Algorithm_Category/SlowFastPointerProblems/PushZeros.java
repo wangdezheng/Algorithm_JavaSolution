f9ba6931f3043b215d00ceb8b6f47874b4ccd15c
wangdezheng <wang793013053@163.com>
2018-01-16 22:44:36 +0800

    Given an array of random numbers, push all the zero's of a given array to the end of the array

--- Algorithm_Category/SlowFastPointerProblems/PushZeros.java
+++ Algorithm_Category/SlowFastPointerProblems/PushZeros.java
@@ -3,16 +3,17 @@ package SlowFastPointerProblems;
 /**
  * Given an array of random numbers, push all the zero's of a given array to the end of the array.
  * For example, the given array is {1, 9, 8, 4, 0, 0, 2, 7, 0, 6, 0}, it should be changed to
- * {1, 9, 8, 4, 2, 7, 6, 0, 0, 0, 0}. The order of all other elements can be changed.
+ * {1, 9, 8, 4, 2, 7, 6, 0, 0, 0, 0}.
  * Expected time complexity is O(n) and extra space is O(1)
  */
 
+//The order of all other elements can be changed.
 //left index: All elements to the left hand side of left(not including left) are non-zeros.
 // right index: All elements to the right hand side of right(not including right) are zeros.
-public class PushZerosI {
-    public int pushZerosI(int[] input) {
+public class PushZeros {
+    public void pushZeros(int[] input) {
         if (input == null || input.length <= 1) {
-            return -1;
+            return;
         }
         int left = 0;
         int right = input.length - 1;
@@ -28,7 +29,6 @@ public class PushZerosI {
                 right--;
             }
         }
-        return left;
     }
 
     private void swap(int left, int right, int[] input) {
@@ -36,4 +36,26 @@ public class PushZerosI {
         input[left] = input[right];
         input[right] = temp;
     }
+
+
+    //The order of all other elements can't be changed.
+    //slow: all elements to the left hand side of slow(not including slow) are non-zeros
+    //fast index: current index
+//    public void pushZeros(int[] input) {
+//        if (input == null || input.length <= 1) {
+//            return;
+//        }
+//        int slow = 0;
+//        int fast = 0;
+//        while (fast < input.length) {
+//            if (input[fast] != 0) {
+//                input[slow++] = input[fast++];
+//            } else {
+//                fast++;
+//            }
+//        }
+//        while (slow < input.length) {
+//            input[slow++] = 0;
+//        }
+//    }
 }
