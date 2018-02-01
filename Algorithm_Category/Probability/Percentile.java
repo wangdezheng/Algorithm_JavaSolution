d6c7d941eadeadf4e92d7f8a0376cd703803e991
wangdezheng <wangdezheng>
2018-02-01 11:07:14 +0800

    Given a list of integers representing the lengths of urls, find the 95 percentile of all lengths

---
+++ Algorithm_Category/Probability/Percentile.java
@@ -0,0 +1,20 @@
+package Probability;
+
+import java.util.List;
+
+/**
+ * Given a list of integers representing the lengths of urls, find the 95 percentile of all lengths (95% of the urls have lengths <= returned length).
+ *
+ * Assumptions:
+ *      The maximum length of valid url is 4096
+ *      The list is not null and is not empty and does not contain null
+ *
+ * Examples:
+ *      [1, 2, 3, ..., 95, 96, 97, 98, 99, 100], 95 percentile of all lengths is 95.
+ */
+public class Percentile {
+    public int percentile95(List<Integer> lengths) {
+        // Write your solution here.
+        return 0;
+    }
+}
