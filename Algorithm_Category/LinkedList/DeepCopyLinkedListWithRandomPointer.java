98128940fc15ac7e3f5694afca1d4d3a01210b80
wangdezheng <wang793013053@163.com>
2018-02-05 15:28:07 +0800

    Make a deep copy of the original list with a random pointer

---
+++ Algorithm_Category/LinkedList/DeepCopyLinkedListWithRandomPointer.java
@@ -0,0 +1,35 @@
+package LinkedList;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Each of the nodes in the linked list has another pointer pointing to a random node in the list or null. Make a deep copy of the original list.
+ */
+public class DeepCopyLinkedListWithRandomPointer {
+    public ListNode deepCopy(ListNode head) {
+        if (head == null) {
+            return null;
+        }
+        Map<ListNode, ListNode> lookup = new HashMap<>();
+        ListNode newHead = new ListNode(head.value);
+        ListNode cur = newHead;
+        while (head != null) {
+            if (head.next != null) {
+                if (!lookup.containsKey(head.next)) {
+                    lookup.put(head.next, new ListNode(head.next.value));
+                }
+                cur.next = lookup.get(head.next);
+            }
+            if (head.random != null) {
+                if (!lookup.containsKey(head.random)) {
+                    lookup.put(head.random, new ListNode(head.random.value));
+                }
+                cur.random = lookup.get(head.random);
+            }
+            head = head.next;
+            cur = cur.next;
+        }
+        return newHead;
+    }
+}
