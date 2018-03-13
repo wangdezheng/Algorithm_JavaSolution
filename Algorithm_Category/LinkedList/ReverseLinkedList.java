ed221363f0f568f6c1b6eac2e7af5bac6de8ee32
wangdezheng <wang793013053@163.com>
2018-03-13 09:56:32 +0800

    Update for reversing a singly-linked list

--- Algorithm_Category/LinkedList/ReverseLinkedList.java
+++ Algorithm_Category/LinkedList/ReverseLinkedList.java
@@ -1,6 +1,16 @@
 package LinkedList;
 
-public class ReverseLinkedList {
+/**
+ *Reverse a singly-linked list.
+
+ Examples:
+    L = null, return null
+    L = 1 -> null, return 1 -> null
+    L = 1 -> 2 -> 3 -> null, return 3 -> 2 -> 1 -> null
+ */
+
+//Time: O(n), Space: O(n)
+//public class ReverseLinkedList {
 //    public ListNode reverseLinkedList(ListNode head){
 //        if (head == null || head.next == null) {
 //            return head;
@@ -11,10 +21,12 @@ public class ReverseLinkedList {
 //        return  newHead;
 //    }
 
+//Time: O(n), Space: O(1)
+public class ReverseLinkedList {
     public ListNode reverseLinkedList(ListNode head){
         ListNode cur = head;
         ListNode pre = null;
-        while (cur != null || cur.next != null) {
+        while (cur != null) {
             ListNode next = cur.next;
             cur.next = pre;
             pre = cur;
