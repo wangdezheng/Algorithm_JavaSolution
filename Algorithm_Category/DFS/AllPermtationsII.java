8c31eff8519805913d28ee9fb34a1359f4ebc90a
wangdezheng <wang793013053@163.com>
2018-03-27 10:14:33 +0800

    All permutations of string with possible duplicated characters

--- Algorithm_Category/DFS/AllPermtationsII.java
+++ Algorithm_Category/DFS/AllPermtationsII.java
@@ -14,6 +14,7 @@ import java.util.List;
     Set = null, all permutations are []
  */
 
+//Time: O(n!), Space: O(n)
 public class AllPermtationsII {
     public List<String> permutations(String set) {
         List<String> result = new ArrayList<>();
