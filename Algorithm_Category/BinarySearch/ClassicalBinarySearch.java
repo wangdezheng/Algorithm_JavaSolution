f837aada3675dc2664b03c9c86d7bdd3b8cc1d29
Tom Wang <wang793013053@163.com>
2020-10-20 23:23:20 +0800

    modify description for binary search

--- Algorithm_Category/BinarySearch/ClassicalBinarySearch.java
+++ Algorithm_Category/BinarySearch/ClassicalBinarySearch.java
@@ -2,10 +2,9 @@ package BinarySearch;
 
 /**
  * Given a target integer T and an integer array A sorted in ascending order, find the index i such that A[i] == T or return -1 if there is no such index.
- * <p>
- * Assumptions
- * <p>
- * There can be duplicate elements in the array, and you can return any of the indices i such that A[i] == T.
+ *
+ * Assumptions:
+ *  There can be duplicate elements in the array, and you can return any of the indices i such that A[i] == T.
  */
 
 public class ClassicalBinarySearch {
